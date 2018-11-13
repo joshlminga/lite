34e2c4a4c4a697c625217488bf928215704da9ec
Josh L. Minga <joshlminga@gmail.com>
2018-11-13 22:43:16 +0300

    Configure Codeigniter

---
+++ application/controllers/Core_Main.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Core_Main extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Do your magic here
+	}
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Core_Main.php */
+/* Location: ./application/controllers/Core_Main.php */
