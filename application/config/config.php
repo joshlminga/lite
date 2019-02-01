827d356b341322786d629a6380719524e781fcce
Josh L. Minga <joshlminga@gmail.com>
2019-02-01 21:32:05 +0300

    Base URL Updated

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PROJECTS/Core_CMS_Lite/';
+$config['base_url'] = 'http://localhost/PROJECT2019/Core_CMS_Lite/';
 
 /*
 |--------------------------------------------------------------------------
