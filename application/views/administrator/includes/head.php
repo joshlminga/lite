6ebea012bb86ed2368513a33f3ef0e2c54682b7e
Josh L. Minga <joshlminga@gmail.com>
2019-02-04 11:26:51 +0300

    Menu To Work on Mobile

--- application/views/administrator/includes/head.php
+++ application/views/administrator/includes/head.php
@@ -54,11 +54,6 @@
             </div>
 
             <ul class="pull-right h-menu">
-                <li class="hm-search-trigger">
-                    <a class="core-sidebar-link" href="#" data-ma-action="search-open">
-                        <i class="hm-icon zmdi zmdi-search"></i>
-                    </a>
-                </li>
                 <li class="dropdown hidden-xs">
                     <a data-toggle="dropdown" href="#"><i class="hm-icon zmdi zmdi-more-vert"></i></a>
                     <ul class="dropdown-menu dm-icon pull-right">
@@ -66,9 +61,6 @@
                             <a data-action="fullscreen" href="#"><i class="zmdi zmdi-fullscreen"></i> Toggle Fullscreen</a>
                         </li>
                     </ul>
-                </li>
-                <li class="hm-alerts" data-user-alert="sua-messages" data-ma-action="sidebar-open" data-ma-target="user-alerts">
-                    <a href="#"><i class="hm-icon zmdi zmdi-notifications"></i></a>
                 </li>
                 <li class="dropdown hm-profile">
                     <a data-toggle="dropdown" href="#">
@@ -91,7 +83,7 @@
                 </li>
             </ul>
             
-            <div class="media-body h-search">
+            <div class="media-body h-search hidden-xs hidden-sm">
                 <div class="p-relative">
                     <span class="welcome-header">
                         Welcome to Core Lite, you're running Lite version 1.0 | Build Faster &amp; Smart.
