d703ce870c91b60afa5fc8826258eade3d20a12e
joshlminga <joshlminga@gmail.com>
2019-10-16 21:48:12 +0300

    Version 4.42

--- application/models/CoreLoad.php
+++ application/models/CoreLoad.php
@@ -36,8 +36,8 @@ class CoreLoad extends CI_Model {
     {
 
 		//Loading Core CMS Version
-		$data['version'] = '4.4';
-		$data['copyright_footer_1'] = "Copyright &copy; 2019 Core Lite ".$data['version']." | Published 15-Oct-2019";
+		$data['version'] = '4.42';
+		$data['copyright_footer_1'] = "Copyright &copy; 2019 Core Lite ".$data['version']." | Published 16-Oct-2019";
 		$data['copyright_footer_2'] = "Powered by Core-Lite Team";
 
     	//Values Assets
