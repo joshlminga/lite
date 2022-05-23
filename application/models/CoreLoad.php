02def5c89d5b3ef61341aac649f4f5c32832512c
Josh L. Minga <joshlminga@gmail.com>
2022-05-23 21:57:54 +0300

    Version update

--- application/models/CoreLoad.php
+++ application/models/CoreLoad.php
@@ -38,9 +38,9 @@ class CoreLoad extends CI_Model
 	{
 
 		//Loading Core CMS Version
-		$data['version'] = '4.87';
+		$data['version'] = '4.89';
 		$data['copyright_footer_1'] = "v" . $data['version'];
-		$data['copyright_footer_2'] = "Published 25-MAR-2022";
+		$data['copyright_footer_2'] = "Published 23-MAY-2022";
 
 		//Values Assets
 		$data['assets'] = $this->CoreCrud->selectSingleValue('settings', 'value', array('title' => 'assets', 'flg' => 1));
