b2a8408e713e306c6c8c15ffd156806fb8b8b644
Josh L. Minga <joshlminga@gmail.com>
2022-10-07 13:41:25 +0300

    CoreNotify updated to support redirect error notifications

--- application/models/CoreLoad.php
+++ application/models/CoreLoad.php
@@ -38,9 +38,9 @@ class CoreLoad extends CI_Model
 	{
 
 		//Loading Core CMS Version
-		$data['version'] = '5.45';
+		$data['version'] = '5.46';
 		$data['copyright_footer_1'] = "v" . $data['version'];
-		$data['copyright_footer_2'] = "Published 02-SEP-2022";
+		$data['copyright_footer_2'] = "Published 07-OCT-2022";
 
 		//Values Assets
 		$data['assets'] = $this->CoreCrud->selectSingleValue('settings', 'value', array('title' => 'assets', 'flg' => 1));
